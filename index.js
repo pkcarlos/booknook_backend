549fa8bb78397090f463473edeb8b5164b9325e2
pkcarlos <patricia.k.carlos@gmail.com>
2025-02-01 19:08:52 -0800

    adding frontend to backend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 app.use((express.json())) // need to use this as middleware because express doesn't parse json automatically
 app.use(cors())
+app.use(express.static('dist'))
 
 let books = [
   {
@@ -112,7 +113,7 @@ app.post('/api/books', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
